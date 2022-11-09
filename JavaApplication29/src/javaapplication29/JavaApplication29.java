5e1e7516268039da51645c56571754c0000b16af
TuralJSDeveloper <tural.azizxanli92@gmail.com>
2022-11-09 22:59:55 +0400

    Java Types

---
+++ JavaApplication29/src/javaapplication29/JavaApplication29.java
@@ -0,0 +1,21 @@
+package javaapplication29;
+
+/**
+ *
+ * @author Tural
+ */
+public class JavaApplication29 {
+
+    public static void main(String[] args) {
+        int a; //Integer
+        double b; //Double
+        long c; //Long
+        char d; //Character
+        String e; //String
+        float f; //Float
+        boolean g; //Boolean
+        byte h; //Byte
+        
+    }
+
+}
